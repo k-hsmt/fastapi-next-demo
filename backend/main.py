632ee0c39fe2a9ddf331543d50fe223a5424a468
tanaka.taro <tanaka.taro@ism.com>
2025-09-04 16:48:39 +0900

    CORSエラーの解消＆コメント修正

--- backend/main.py
+++ backend/main.py
@@ -6,15 +6,16 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",      # 開発中のNext.js
     "http://127.0.0.1:3000",
-    "https://fastapi-next-demo.vercel.app/",
+    "https://fastapi-next-demo.vercel.app",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_origin_regex=r"https://.*\.vercel\.app",  # プレビュー用サブドメインも許可
     allow_credentials=True,   # 認証付きfetchを想定しないなら True/FalseどちらでもOK
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"], # または ["content-type", "authorization", ...]
 )
 
 @app.get("/healthz")
